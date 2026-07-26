37a47926a8ded50e7866b5afdd9cee3f1f98ceb5
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-07-26 18:29:45 +0700

    zxcvbnm

--- src/installer.ts
+++ src/installer.ts
@@ -2,6 +2,7 @@ import * as os from 'os';
 import * as path from 'path';
 import * as util from 'util';
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 import * as httpm from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
 
@@ -31,83 +32,91 @@ export const getRelease = async (version: string): Promise<GitHubRelease> => {
   return releases[version];
 };
 
-function getPlatformCandidates(): string[] {
-  // Node reports macOS as "darwin"; some release assets may use "macos".
-  const base = osArch === 'x64'
-    ? (osPlat === 'win32' ? 'win64' : `amd64`)
-    : osArch === 'x32'
-      ? (osPlat === 'win32' ? 'win32' : `i386`)
-      : osArch === 'arm'
-        ? (() => {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const armVersion = (process.config.variables as any).arm_version;
-            return armVersion === '7' ? 'armeb' : 'arm';
-          })()
-        : osArch;
+const PLATFORM_TOKENS: Record<string, string> = {
+  'win32:x64': 'win64',
+  'win32:ia32': 'win32',
+  'linux:x64': 'amd64_linux',
+  'linux:ia32': 'i386_linux',
+  'linux:arm64': 'arm64_linux',
+  'linux:mips': 'mips_linux',
+  'linux:mipsel': 'mipsel_linux',
+  'linux:ppc': 'powerpc_linux',
+  'linux:ppc64': 'powerpc64le_linux',
+  'linux:riscv64': 'riscv64_linux'
+};
 
-  if (osPlat === 'darwin') {
-    return [`${base}_darwin`, `${base}_macos`];
-  }
-
-  return [`${base}_${osPlat}`];
+function getLinuxArmToken(): string {
+  const armVersion = (process.config.variables as any).arm_version;
+  return armVersion === '7' ? 'armeb_linux' : 'arm_linux';
 }
 
-function getName(version: string, platform: string): string {
-  return util.format('upx-%s-%s', version, platform);
+function getPlatformToken(): string | undefined {
+  if (osPlat === 'linux' && osArch === 'arm') {
+    return getLinuxArmToken();
+  }
+  return PLATFORM_TOKENS[`${osPlat}:${osArch}`];
+}
+
+function getName(version: string, platformToken: string): string {
+  return util.format('upx-%s-%s', version, platformToken);
+}
+
+function unsupportedPlatformError(): Error {
+  return new Error(`Could not find a matching UPX asset for ${osPlat}/${osArch}`);
+}
+
+async function installUpxWithBrew(): Promise<string> {
+  core.startGroup('Installing UPX with Homebrew');
+  try {
+    await exec.exec('brew', ['install', 'upx']);
+  } finally {
+    core.endGroup();
+  }
+
+  return '/opt/homebrew/bin/upx';
 }
 
 export async function getUPX(version: string): Promise<string> {
+  if (osPlat === 'darwin') {
+    return await installUpxWithBrew();
+  }
+
   const release: GitHubRelease = await getRelease(version);
   const semver: string = release.tag_name.replace(/^v/, '');
   core.info(`UPX ${semver} found`);
 
-  const candidates = getPlatformCandidates();
+  const platformToken = getPlatformToken();
+  if (!platformToken) {
+    throw unsupportedPlatformError();
+  }
+
   const ext = osPlat === 'win32' ? 'zip' : 'tar.xz';
-
-  let downloadUrl: string | undefined;
-  let extractedPath: string | undefined;
-  let cachePath: string | undefined;
-
-  for (const platform of candidates) {
-    const filename = `${getName(semver, platform)}.${ext}`;
-    const candidateUrl = `https://github.com/upx/upx/releases/download/v${semver}/${filename}`;
-
-    try {
-      core.startGroup(`Downloading ${candidateUrl}...`);
-
-      const downloadPath: string = await tc.downloadTool(candidateUrl);
-      core.info(`Downloaded to ${downloadPath}`);
-
-      if (osPlat === 'win32') {
-        extractedPath = await tc.extractZip(downloadPath);
-      } else {
-        extractedPath = await tc.extractTar(downloadPath, undefined, 'x');
-      }
-      core.info(`Extracted to ${extractedPath}`);
-
-      cachePath = await tc.cacheDir(extractedPath, 'ghaction-upx', semver);
-      core.debug(`Cached to ${cachePath}`);
-
-      downloadUrl = candidateUrl;
-      break;
-    } catch (err) {
-      core.warning(`Failed with ${platform}: ${(err as Error).message}`);
-      core.endGroup();
-    }
+  const assetName = getName(semver, platformToken);
+  const downloadUrl = release.assets.find(asset => asset.endsWith(`/${assetName}.${ext}`));
+  if (!downloadUrl) {
+    throw new Error(
+      `UPX ${semver} has no "${assetName}.${ext}" asset. See ${release.html_url} for what is actually published.`
+    );
   }
 
-  if (!downloadUrl || !cachePath) {
-    throw new Error(`Could not find a matching UPX asset for ${osPlat}/${osArch}`);
+  core.startGroup(`Downloading ${downloadUrl}...`);
+  let cachePath: string;
+  try {
+    const downloadPath = await tc.downloadTool(downloadUrl);
+    core.info(`Downloaded to ${downloadPath}`);
+
+    const extractedPath =
+      osPlat === 'win32' ? await tc.extractZip(downloadPath) : await tc.extractTar(downloadPath, undefined, 'x');
+    core.info(`Extracted to ${extractedPath}`);
+
+    cachePath = await tc.cacheDir(extractedPath, 'ghaction-upx', semver);
+    core.debug(`Cached to ${cachePath}`);
+  } finally {
+    core.endGroup();
   }
 
-  const selectedPlatform = candidates.find((platform) => {
-    const exePath = path.join(cachePath!, getName(semver, platform), osPlat === 'win32' ? 'upx.exe' : 'upx');
-    return true;
-  }) ?? candidates[0];
-
-  const exePath = path.join(cachePath, getName(semver, selectedPlatform), osPlat === 'win32' ? 'upx.exe' : 'upx');
+  const exePath = path.join(cachePath, assetName, osPlat === 'win32' ? 'upx.exe' : 'upx');
   core.debug(`Exe path is ${exePath}`);
-  core.endGroup();
 
   return exePath;
 }
